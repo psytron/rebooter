620841645919fdc72acdd0025af7f0d4b0ff5029
Psy Tron <m@psytron.com>
2023-12-02 16:46:55 -0800

    first rebooter sketch

---
+++ rebooter.py
@@ -0,0 +1,35 @@
+
+
+
+frontier_status():
+	res = requests.load("https://google.com"):
+	if res.status == 200:
+		print('Success ')
+	else:
+		return False;
+
+
+power_status():
+	gpio.pin17.status
+	return True;
+
+
+wifi_status():
+	network_io.signal_value:
+	return True;
+
+
+
+
+power = powerStatus() 
+wifi = checkSignal()
+frontier = loadGoogle()
+
+
+
+if( wifi_signal == 'good' && frontier_response =='fail'): 
+	power_off() 
+
+
+
+How are the herbs and  
